a790db4a3429a8cd9679eee7a6c60f8fadfecf3b
medreen.wc <medreenwchege@gmail.com>
2026-02-24 21:48:20 +0300

    sqlalchemy session_post

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 from flask import Flask, jsonify, request
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database import Base, User
 
 app = Flask(__name__)
 
-allowed_methods = ["GET", "POST", "PUT", "PATCH", "UPDATE", "DELETE"]
+DATABASE_URL = "postgresql+psycopg2://postgres:Colesprouse2311!@localhost:5432/flask_api"
+engine = create_engine(DATABASE_URL, echo=True)
+
+session = sessionmaker(bind=engine)
+my_session = session()
+
+Base.metadata.create_all(engine)
+
+allowed_methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
 users_list = [
 
 ]
@@ -26,7 +37,10 @@ def users():
             if data["name"] == "" or data["location"] == "":
                 return jsonify({"message": "name and location fields required."}), 403
             else:
-                users_list.append(data)
+                #users_list.append(data)                                
+                new_user = User(name = data["name"], location = data["location"])
+                my_session.add(new_user)
+                my_session.commit()
                 return jsonify({"message": "Successfully added users"}), 201
         else:
             return jsonify({"message": "Methods not allowed."}), 405
