11758925e20e16723654b25a4365352281ac5408
medreen.wc <medreenwchege@gmail.com>
2026-02-24 22:47:25 +0300

    modified main to read users

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from flask import Flask, jsonify, request
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, select
 from sqlalchemy.orm import sessionmaker
 from database import Base, User
 
 app = Flask(__name__)
 
 DATABASE_URL = "postgresql+psycopg2://postgres:Colesprouse2311!@localhost:5432/flask_api"
-engine = create_engine(DATABASE_URL, echo=True)
+engine = create_engine(DATABASE_URL, echo=False)
 
 session = sessionmaker(bind=engine)
 my_session = session()
@@ -14,9 +14,7 @@ my_session = session()
 Base.metadata.create_all(engine)
 
 allowed_methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-users_list = [
 
-]
 
 @app.route("/", methods = allowed_methods)
 def home():
@@ -31,6 +29,15 @@ def users():
     try:
         method = request.method.lower()
         if method == "get":
+            users_list = []
+            query = select(User)
+            my_users = list(my_session.scalars(query).all())
+
+            for user in my_users:
+                users_list.append({"id": user.id,
+                                    "name": user.name,
+                                    "location": user.location})
+
             return jsonify({"data": users_list}), 200
         elif method == "post":
             data = request.get_json()
