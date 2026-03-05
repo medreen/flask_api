085815e77622315bb55c123f38d2ebf5e77e4449
medreen.wc <medreenwchege@gmail.com>
2026-03-05 21:30:28 +0300

    added register route

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
 import select
-
 from flask import Flask, request, jsonify
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from database import User, db
+from flask_cors import CORS
 
 app = Flask(__name__)
 
-alloweed_methods = ["GET", "POST", "PUT", "DELETE", "PATCH"]
+#instatiate cors
+CORS(app, origins=['http://127.0.0.1:5500'])
+
+allowed_methods = ["GET", "POST", "PUT", "DELETE", "PATCH"]
 
 DATABASE_URL = "postgresql+psycopg2://postgres:Colesprouse2311!@localhost:5432/vue_myduka"
 
 # Connect to the database using sqlalchemy
-engine = create_engine(DATABASE_URL, echo=True)
+engine = create_engine(DATABASE_URL, echo=False)
 
 # Create a session to call query methods
 session = sessionmaker(bind=engine)
@@ -21,10 +24,7 @@ my_session = session()
 # Create the tables in the database
 db.metadata.create_all(engine)
 
-
-
-
-@app.route("/", methods=alloweed_methods)
+@app.route("/", methods=allowed_methods)
 def home():
     if request.method == "GET":
         msg = { "Flask API Version" : "1.0" }
@@ -33,7 +33,7 @@ def home():
         return jsonify({"error": "Method not allowed"}), 405
 
 
-@app.route("/user")
+@app.route("/user", methods= allowed_methods)
 def user():
     if request.method.upper() == "GET":
         # return a list of all users in the database
@@ -45,17 +45,30 @@ def user():
             data.append({
                 "id": user.id,
                 "name": user.name,
-                "location": user.location
+                "email": user.email #change email to location
             })
         return jsonify({ "data": data })
     elif request.method.upper() == "POST":
         data = request.get_json()
-        if data["Name"] == "" or data["Location"] == "":
-            return jsonify({ "error": "Name and Location cannot be empty" }), 400
+        if data["Name"] == "" or data["Email"] == "":
+            return jsonify({ "error": "Name and Email cannot be empty" }), 400
         else:
-            new_user = User(name=data["Name"], location=data["Location"])
+            new_user = User(name=data["Name"], email=data["Email"])
             my_session.add(new_user)
             my_session.commit()
             return jsonify({ "message": f"User created successfully{data['name']}" }), 201
-    
+
+
+@app.route("/register", methods=allowed_methods)
+def register_user():
+    data = request.get_json()
+    if data["username"] == "" or data["email"] == "" or data["password"] == "":
+        return jsonify({ "error": "Enter the required fields" }), 400
+    else:
+        new_user = User(username=data["username"], email=data["email"], password=data["password"])
+        my_session.add(new_user)
+        my_session.commit()
+        return jsonify({ "message": f"User created successfully{data['username']}" }), 201
+ 
+
 app.run(debug=True)
