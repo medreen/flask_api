c16888f3f8c9e7b67475f9dce9d16f0d7f6a35a8
medreen.wc <medreenwchege@gmail.com>
2026-04-01 14:44:13 +0300

    updated main and db

--- database.py
+++ database.py
@@ -1,10 +1,32 @@
-from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
+from sqlalchemy import Integer, String, Float, DateTime, ForeignKey
 
-db = SQLAlchemy()
 
-class User(db.Model):
-    __tablename__ = 'users'
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(50), nullable=False)
-    email = db.Column(db.String(120), nullable = False, unique = True)
-    password = db.Column(db.String(255), nullable= False)
+class Base(DeclarativeBase):
+    pass
+
+class User(Base):
+    __tablename__ = "Users"
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    username: Mapped[str] = mapped_column(String(50), nullable=False)
+    email: Mapped[str] = mapped_column(String(50), unique=True, nullable=False)
+    password: Mapped[str] = mapped_column(String(255), nullable=False)
+    created_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.utcnow)
+
+    # Relationship
+    budgets = relationship("User_Budget", back_populates="Users")
+
+class Budget(Base):
+    __tablename__ = "User_Budget"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    title: Mapped[str] = mapped_column(String(50), nullable=False)
+    amount: Mapped[float] = mapped_column(Float, nullable=False)
+    date: Mapped[datetime] = mapped_column(DateTime, default=datetime.utcnow)
+
+    # Foreign Key
+    user_id: Mapped[int] = mapped_column(ForeignKey("Users.id"))
+
+    # Relationship
+    user = relationship("Users", back_populates="User_Budgets")
--- main.py
+++ main.py
@@ -1,74 +1,176 @@
-import select
 from flask import Flask, request, jsonify
+from database import Base, Budget, User 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from database import User, db
+from datetime import datetime
+from flask_bcrypt import Bcrypt
+from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
 from flask_cors import CORS
 
 app = Flask(__name__)
 
-#instatiate cors
-CORS(app, origins=['http://127.0.0.1:5500'])
+CORS(app)
 
-allowed_methods = ["GET", "POST", "PUT", "DELETE", "PATCH"]
+app.config['JWT_SECRET_KEY'] = 'medreen3435!'
 
-DATABASE_URL = "postgresql+psycopg2://postgres:Colesprouse2311!@localhost:5432/vue_myduka"
+bcrypt = Bcrypt(app)
+jwt = JWTManager(app)
 
-# Connect to the database using sqlalchemy
-engine = create_engine(DATABASE_URL, echo=False)
+DATABASE_URL = "postgresql://postgres:Colesprouse2311!@localhost:5432/budgets"
 
-# Create a session to call query methods
-session = sessionmaker(bind=engine)
-my_session = session()
+engine = create_engine(DATABASE_URL)
+Base.metadata.create_all(engine)
 
-# Create the tables in the database
-db.metadata.create_all(engine)
+SessionLocal = sessionmaker(bind=engine)
+db_session = SessionLocal()
 
-@app.route("/", methods=allowed_methods)
-def home():
-    if request.method == "GET":
-        msg = { "Flask API Version" : "1.0" }
-        return jsonify(msg), 200
-    else:
-        return jsonify({"error": "Method not allowed"}), 405
+allowed_methods = ['POST', 'GET', 'PUT', 'DELETE','PATCH']
 
+@app.route('/register', methods=allowed_methods)
+def register():
+    try:
+        if request.method == "POST":            
+            data = request.get_json()
 
-@app.route("/user", methods= allowed_methods)
-def user():
-    if request.method.upper() == "GET":
-        # return a list of all users in the database
-        query = select(User)
-        users = my_session.scalars(query).all()
-        data = []
+            if data:
+                username = data.get('username')
+                email = data.get('email')
+                password = data.get('password')           
+            else:
+                return jsonify({"message": "Request body is empty"}), 400
+            
+            if username == '' or password == '' or email=='':
+                return jsonify({"error": "All fields required!"}), 400
+            else:
+                email = email.lower()
+                # validate the user
+                existing_user = db_session.query(User).filter_by(email=email).first()
+                if existing_user:
+                    return jsonify({"error": "User already exists"}), 409
 
-        for user in users:
-            data.append({
-                "id": user.id,
-                "name": user.name,
-                "email": user.email #change email to location
-            })
-        return jsonify({ "data": data })
-    elif request.method.upper() == "POST":
-        data = request.get_json()
-        if data["Name"] == "" or data["Email"] == "":
-            return jsonify({ "error": "Name and Email cannot be empty" }), 400
+                hashed_password = bcrypt.generate_password_hash(password).decode('utf-8')
+
+                new_user = User(
+                    username=username,
+                    email=email,
+                    password=hashed_password,
+                    created_at=datetime.utcnow()
+                )
+
+                db_session.add(new_user)
+                db_session.commit()
+
+                token = create_access_token(identity=new_user.email)
+
+                return jsonify({
+                    "message": "User registered successfully!",
+                    "token": token
+                }), 201
+        else: 
+            return jsonify({"message": "Method not allowed"})
+
+    except Exception as e:        
+        return jsonify({"error": str(e)}), 500
+
+@app.route('/login', methods=allowed_methods)
+def login():
+    try:
+        if request.method == "POST":            
+            data = request.get_json()
+
+            if data:               
+                email = data.get('email')
+                password = data.get('password')           
+            else:
+                return jsonify({"message": "Request body is empty"}), 400
+            
+            if password == '' or email == '':
+                return jsonify({"error": "All fields required!"}), 400
+            else:
+                 email = email.lower()
+                 user = db_session.query(User).filter_by(email=email).first()
+
+                 if user and bcrypt.check_password_hash(user.password, password):        
+                    token = create_access_token(identity=new_user.email)
+
+                    return jsonify({
+                        "message": "Login successful!",
+                        "token": token
+                    }), 201
+        else: 
+            return jsonify({"message": "Method not allowed"})
+
+    except Exception as e:        
+        return jsonify({"error": str(e)}), 500
+
+@app.route('/budget', methods=allowed_methods)
+@jwt_required()
+def budget():
+    try:
+        if request.method == "POST":            
+            print("JWT identity:", get_jwt_identity())
+
+            current_email = get_jwt_identity()
+        
+            if current_email:                  
+                data = request.get_json()
+            else: 
+                return jsonify({"error": "Unauthorized User"}), 401
+                
+            if data: 
+                title = data.get("title")              
+                amount = data.get('amount')
+                date = data.get('date')   
+                
+            else:
+                return jsonify({"message": "Request body is empty"}), 400
+
+            if title == '' or amount == '' or date == '':
+                return jsonify({"error": "All fields required!"}), 400    
+            else:    
+                try:
+                    date_obj = datetime.strptime(date, "%Y-%m-%d")
+                except:
+                    return jsonify({"error": "Invalid date format"}), 400
+
+                user = db_session.query(User).filter_by(username=current_username).first()
+
+                new_budget = Budget(
+                    title=title,
+                    amount=float(amount),
+                    date=date_obj,
+                    user_id=user.id
+                )
+
+                db_session.add(new_budget)
+                db_session.commit()
+                db_session.close()
+
+                return jsonify({"message": "Budget added"}), 201
+           
+
+        elif request.method == "GET":            
+            current_email = get_jwt_identity()
+
+            user = db_session.query(User).filter_by(email=current_email).first()
+
+            budgets = db_session.query(Budget).filter_by(user_id=user.id).all()
+
+            for b in budgets:
+                return jsonify([                    
+                    {
+                        "id": b.id,
+                        "title": b.title,
+                        "amount": b.amount,
+                        "date": b.date.strftime("%Y-%m-%d")
+                    } 
+                ])
         else:
-            new_user = User(name=data["Name"], email=data["Email"])
-            my_session.add(new_user)
-            my_session.commit()
-            return jsonify({ "message": f"User created successfully{data['name']}" }), 201
+            return jsonify({"message": "Method not allowed"}), 405
+
+    except Exception as e:        
+        return jsonify({"error": str(e)}), 500
 
 
-@app.route("/register", methods=allowed_methods)
-def register_user():
-    data = request.get_json()
-    if data["username"] == "" or data["email"] == "" or data["password"] == "":
-        return jsonify({ "error": "Enter the required fields" }), 400
-    else:
-        new_user = User(username=data["username"], email=data["email"], password=data["password"])
-        my_session.add(new_user)
-        my_session.commit()
-        return jsonify({ "message": f"User created successfully{data['username']}" }), 201
- 
-
-app.run(debug=True)
+if __name__ == '__main__':
+    app.run(debug=True)
